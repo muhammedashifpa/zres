3002b5b115ccdfda645d7a0a1dd678fe5e7a397c
muhammed ashif <ashifmohammed231@gmail.com>
2022-07-01 22:44:11 +0530

    bootstrap installation

--- src/components/pos/Pos.jsx
+++ src/components/pos/Pos.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Pos = () => {
   return (
-    <div>Pos</div>
+    <div>
+        <button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
+<button type="button" class="btn btn-link">Link</button>
+    </div>
   )
 }
 
