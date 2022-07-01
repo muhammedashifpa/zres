fc991bbc4a367b6830450f2da0564119799b7aca
muhammed ashif <ashifmohammed231@gmail.com>
2022-07-02 01:26:23 +0530

    remove unwanted line of code from loging

--- src/components/pos/login/Login.jsx
+++ src/components/pos/login/Login.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import styled from 'styled-components';
-import Modal from '../../utils/Modal/Modal';
 
 const Login = () => {
   return (
     <LoginWrapper>
-        <Modal />
     </LoginWrapper>
   )
 }
