61d02ba5871ee72a9e3ccf8a94718855a91670a9
shaheerkp <kpshaheer123@gmail.com>
2022-07-03 02:31:45 +0530

    crm main and all customer page

--- src/components/pos/main/components/crm/components/customers/Customers.jsx
+++ src/components/pos/main/components/crm/components/customers/Customers.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import PaginationTable from "../../../../../../utils/table/PaginationTable";
 
 const Customers = () => {
   return (
-    <div>Customers</div>
-  )
-}
+    <div class="container">
+      <div class="row justify-content-center">
+        <div
+          style={{
+            width: "90%",
+            height: "55px",
+            backgroundColor: "#d3d3d3",
+          }}
+          class="border rounded-4 "
+        >
+          <h6 className="mt-3 ms-3"><strong>Customer Details</strong> </h6>
+        </div>
+      </div>
+      <div>
+        <PaginationTable />
+      </div>
+    </div>
+  );
+};
 
-export default Customers
+export default Customers;
