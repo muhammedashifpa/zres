e60f521e1cd150ad2bd179930e511403b7a1e061
SHIJU G <shiju@lifology.com>
2022-07-02 12:56:27 +0530

    options page design completed

--- src/components/pos/options/Options.jsx
+++ src/components/pos/options/Options.jsx
@@ -1,15 +1,36 @@
 import React from 'react'
-import MainOption from './components/MainOption'
+import styled from 'styled-components'
 import OrderTakingOption from './components/OrderTakingOption'
+import MainOption from './components/MainOption'
 
 const Options = () => {
   return (
     <>
-    <div>Options</div>
+      {/* <div>Options</div>
     <OrderTakingOption/>
-    <MainOption/>
+    <MainOption/> */}
+
+      <div className="p-3">
+        <OptionsOuter className="row m-0 flex align-items-center justify-content-evenly">
+          <div className="col-3 text-center">
+            <OrderTakingOption />
+          </div>
+          <div className="col-3 text-center">
+            <MainOption />
+          </div>
+        </OptionsOuter>
+      </div>
     </>
-  )
+  );
 }
 
 export default Options
+
+
+const OptionsOuter = styled.div`
+  background: rgba(102, 100, 100, 0.2);
+  width: 100%;
+  height: 95vh;
+  margin: auto;
+  border-radius: 10px;
+`;
--- src/components/pos/options/components/OrderTakingOption.jsx
+++ src/components/pos/options/components/OrderTakingOption.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const OrderTakingOption = () => {
   return (
-    <div>OrderTakingOption</div>
-  )
+    <OrderOptions>
+      <h5 className=''>Order Taking Options</h5>
+    </OrderOptions>
+  );
 }
 
 export default OrderTakingOption
+
+const OrderOptions = styled.div`
+  height: 400px;
+  background: white;
+  border-radius: 20px;
+  padding-top: 50px;
+  box-shadow: 0px 0px 6px 0px rgba(122, 119, 119, 0.75);
+  h5 {
+    color: #040153;
+  }
+`;
