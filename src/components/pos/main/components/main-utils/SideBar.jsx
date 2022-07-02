bed145030463464653e17a928778985a7fbd89b8
muhammed ashif <ashifmohammed231@gmail.com>
2022-07-02 23:08:53 +0530

    code clean

--- src/components/pos/main/components/main-utils/SideBar.jsx
+++ src/components/pos/main/components/main-utils/SideBar.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import { 
     BsFillHouseDoorFill,
@@ -18,11 +17,11 @@ import Nav from './utils/Nav';
 const SideBar = () => {
   return (
     <SideBarWrapper className="d-flex flex-column flex-shrink-0 p-3 bg-light h-100" >
-    <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+    {/* <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
       <svg className="bi pe-none me-2" width="40" height="32"><use xLinkHref="#bootstrap"/></svg>
       <span className="fs-4">Sidebar</span>
     </Link>
-    <hr/>
+    <hr/> */}
     <Nav data={NavData}/>
     <hr/>
     {/* <div className="dropdown">
