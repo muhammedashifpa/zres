e6abf892f53502c9aab11c033a757ad3a3ef23a4
SHIJU G <shiju@lifology.com>
2022-07-03 15:20:09 +0530

    update

--- src/components/pos/main/components/utils/order-interface/components/LeftHead.jsx
+++ src/components/pos/main/components/utils/order-interface/components/LeftHead.jsx
@@ -1,4 +1,3 @@
-import { Link } from '@mui/material';
 import React from 'react'
 
 export const LeftHead = () => {
@@ -13,35 +12,12 @@ export const LeftHead = () => {
         </p>
       </div>
       <div className="row">
-        <p className="col d-flex align-items-start">
-          <span>Order Type :</span>
-          <span className="ms-3">
-            <select
-              className="form-select form-select mb-3"
-              aria-label=".form-select-lg example"
-              style={{ border: "none", width: "100px", height: "30px", padding: " 0px" }}
-            >
-              <option className="" value="Walk in">
-                Walk in
-              </option>
-              <option className="" value="Dine in">
-                Dine in
-              </option>
-            </select>
-          </span>
+        <p className="col">
+          Check :<span>458</span>
         </p>
         <p className="col text-end">
           25 Nov 21 <span className="ms-2">17:58</span>
         </p>
-      </div>
-      <div className="row">
-        <input
-          type="search"
-          className="form-control rounded"
-          placeholder="Search"
-          aria-label="Search"
-          aria-describedby="search-addon"
-        />
       </div>
     </div>
   );
