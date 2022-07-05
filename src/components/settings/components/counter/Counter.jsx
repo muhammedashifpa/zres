8253c42fbba3c0e1ac7bc01eb8ef912eaf16e004
SHIJU G <shiju@lifology.com>
2022-07-06 00:38:05 +0530

    settings page updated
    last three tabs content updated

--- src/components/settings/components/counter/Counter.jsx
+++ src/components/settings/components/counter/Counter.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import { BsFillFileEarmarkFill, BsFillTrashFill, BsPencil } from "react-icons/bs";
 
 const Counter = () => {
   return (
-    <div>Counter</div>
-  )
+    <>
+      <div className="container">
+        <div className="mt-3 text-end">
+          <button className="btn mx-1">
+            <BsFillFileEarmarkFill className="mr-2" /> New
+          </button>
+          <button className="btn mx-1">
+            <BsPencil className="mr-2" /> Edit
+          </button>
+          <button className="btn mx-1">
+            <BsFillTrashFill className="mr-2" /> Delete
+          </button>
+        </div>
+        <div className="row  bg-white mt-3">
+          <table className="table">
+            <thead className="bg-warning">
+              <tr>
+                <th scope="col">SlNo</th>
+                <th scope="col">Counter Name</th>
+                <th scope="col">Terminal</th>
+                <th scope="col">Computer</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Pos</td>
+                <td>server Terminal</td>
+                <td>server</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Counter
