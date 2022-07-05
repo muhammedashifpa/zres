81360f95bd74db0cdaa981fcba5dda12a9bd6f93
SHIJU G <shiju@lifology.com>
2022-07-05 10:01:18 +0530

    setting  tab manager added

--- src/components/settings/components/settingsManager/SettingsManager.jsx
+++ src/components/settings/components/settingsManager/SettingsManager.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { LeftSettingManager } from "./components/LeftSettingManager";
+import { RightSettingManager } from "./components/RightSettingManager";
 
 const SettingsManager = () => {
   return (
-    <div>SettingsManager</div>
-  )
-}
+    <>
+      <div className="container-fluid bg-light mt-4 p-2">
+        <h5 className="">Settings Manager</h5>
+        <div className="row">
+          <LeftSettingManager />
+          <RightSettingManager />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default SettingsManager
+export default SettingsManager;
+
+
