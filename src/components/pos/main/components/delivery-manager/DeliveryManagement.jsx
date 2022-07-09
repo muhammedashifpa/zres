d8af54ec6b68d3b4658dcf32077b6238a2a56623
muhammedashifpa <ashifmohammed231@gmail.com>
2022-07-09 17:17:25 +0530

    added order intereface in develiver manager

--- src/components/pos/main/components/delivery-manager/DeliveryManagement.jsx
+++ src/components/pos/main/components/delivery-manager/DeliveryManagement.jsx
@@ -1,51 +1,19 @@
-import React, { useState } from "react";
-import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
-import styled from "styled-components";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-import Nav from "./components/Nav";
-import HomeDelevery from "./components/homedelvery/HomeDelevery";
-import TakeAwayOrders from "./components/take-away-orders/TakeAwayOrders";
-import FutureOrders from "./components/future-orders/FutureOrders";
+import OrderInterface from "../utils/order-interface/OrderInterface";
+import DeliveryManagementMain from "./components/DeliveryManagementMain";
 
 const Dashbord = () => {
  
   return (
-    <div>
-      <Nav
-        data={TabData2}
-      />
-      <ContentWrapper className="py-3">
-        <Routes>
-            <Route path="" element={<Navigate to='home-delivery-orders' />} />
-            <Route path='home-delivery-orders/*' element={<HomeDelevery />} />
-            <Route path="take-away-orders/*" element={<TakeAwayOrders />} />
-            <Route path="future-orders" element={<FutureOrders/> } />
-        </Routes>
-      </ContentWrapper>
-    </div>
+    <Routes>
+      <Route path='/*' element={<DeliveryManagementMain/>}/>
+      <Route path='new-order' element={<OrderInterface/>}/>
+    </Routes>
+
   );
 };
 
 export default Dashbord;
 
-const ContentWrapper = styled.div``;
-
-
-
-const TabData2 = [
-  {
-    key: 1,
-    name: "Home Delivery Orders",
-    path: "home-delivery-orders",
-  },
-  {
-    key: 2,
-    name: "Take Away Orders",
-    path: "take-away-orders",
-  },
-  {
-    key: 3,
-    name: "Future Orders",
-    path: "future-orders",
-  },
-];
