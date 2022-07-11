c3df2bc7c3af4313bcf61d04e343be9a5931a304
muhammedashifpa <ashifmohammed231@gmail.com>
2022-07-11 19:00:49 +0530

    updates on dine in

--- src/components/pos/main/components/dine-in/DineIn.jsx
+++ src/components/pos/main/components/dine-in/DineIn.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import CompletedOrders from './components/complete-orders/CompletedOrders'
+import Footer from './components/Footer'
 import Header from './components/Header'
+import RunningOrders from './components/running-orders/RunningOrders'
+import Tables from './components/tables/Tables'
 
 
 
@@ -7,6 +12,13 @@ const DineIn = () => {
   return (
     <div>
       <Header/>
+        <Routes>
+          <Route path='' element={<Navigate to='tables'/>}/>
+          <Route path='tables' element={<Tables/>}/>
+          <Route path='running-orders' element={<RunningOrders/>}/>
+          <Route path='compleated-orders' element={<CompletedOrders/>}/>
+        </Routes>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/pos/main/components/dine-in/components/running-orders/RunningOrders.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RunningOrders = () => {
+  return (
+    <div>RunningOrders</div>
+  )
+}
+
+export default RunningOrders
