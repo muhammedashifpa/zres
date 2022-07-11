c79f2282ae357ac02e5b104e7569370635cdf9e9
muhammedashifpa <ashifmohammed231@gmail.com>
2022-07-11 22:03:14 +0530

    bussinese summary

--- src/components/pos/main/components/dashbord/business-summary/BusinessSummary.jsx
+++ src/components/pos/main/components/dashbord/business-summary/BusinessSummary.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const BusinessSummary = () => {
   return (
-    <div>BusinessSummary</div>
+    <div className="container">
+      <div className="row">
+        <div className="col-4">
+          
+        </div>
+        <div className="col-8">
+
+        </div>
+      </div>
+    </div>
   )
 }
 
