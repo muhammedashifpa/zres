dc640d17d122141cdc80dad86b5ee5a6857dd332
muhammedashifpa <ashifmohammed231@gmail.com>
2022-07-11 18:04:18 +0530

    modal set up

--- src/components/pos/main/components/accounts/AccountsMenuModal.jsx
+++ src/components/pos/main/components/accounts/AccountsMenuModal.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import styled from 'styled-components';
 import Modal from '../../../../utils/Modal/Modal';
 
 const AccountsMenuModal = () => {
@@ -25,11 +26,18 @@ export default AccountsMenuModal
 
 const AccountsButtons = () => {
   return(
-    <div className='d-flex flex-column py-5'>
-    <Link to='masters'   className='btn btn-primary py-3 mb-3'>Masters</Link>
-    <Link to='transactions'  className='btn btn-primary py-3 mb-3'>Transactions</Link>
-    <Link to='reports'  className='btn btn-primary py-3 mb-3'>Reports</Link>
-    <Link to='chart-of-account'  className='btn btn-primary py-3 mb-3'>Chart of Account</Link>
-    </div>
+    <Wrapper className='d-flex flex-column p-5'>
+      <Link to='masters'   className='btn btn-primary py-3  mb-3'>Masters</Link>
+      <Link to='transactions'  className='btn btn-primary py-3 mb-3'>Transactions</Link>
+      <Link to='reports'  className='btn btn-primary py-3 mb-3'>Reports</Link>
+      <Link to='chart-of-account'  className='btn btn-primary py-3 mb-3'>Chart of Account</Link>
+    </Wrapper>
   )
 }
+
+
+const Wrapper = styled.div`
+  .btn{
+    min-width: 250px;
+  }
+`
